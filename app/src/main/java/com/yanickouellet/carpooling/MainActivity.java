74a656d651b99a2cf039fd81781921f2f919b2f9
Yanick Ouellet <yanickouellet555@gmail.com>
2015-03-01 16:00:08 -0500

    Remove useless menu setting

--- app/src/main/java/com/yanickouellet/carpooling/MainActivity.java
+++ app/src/main/java/com/yanickouellet/carpooling/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends RoboActionBarActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mToolbar.setLogo(R.drawable.ic_drawer);
-        setSupportActionBar(mToolbar);
+        //mToolbar.setLogo(R.drawable.ic_drawer);
+        //setSupportActionBar(mToolbar);
 
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,
@@ -64,28 +64,6 @@ public class MainActivity extends RoboActionBarActivity implements
     public void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mDrawerToggle.syncState();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
